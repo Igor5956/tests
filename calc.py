51aa1e4b10fc8e2ec38fdc759a1475a67a401c9a
Igor5956 <96588200+Igor5956@users.noreply.github.com>
2022-04-19 20:30:55 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,4 @@
+def add(x, y):
+    return x + y
+def is_positive(x):
+    return x > 0
---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+import calc
+class TestCalc(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(calc.add(3, 7), 10)
+    def test_is_positive(self):
+        self.assertTrue(calc.is_positive(3))
+
+if __name__ == "__main__":    unittest.main()
