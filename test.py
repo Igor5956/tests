1041c7d374d91de16d71321ccb63a697af54dc9b
Igor5956 <96588200+Igor5956@users.noreply.github.com>
2022-04-23 20:17:49 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,24 @@
-import unittest
-import calc
-class TestCalc(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(calc.add(3, 7), 10)
-    def test_is_positive(self):
-        self.assertTrue(calc.is_positive(3))
+import unittest
 
-if __name__ == "__main__":    unittest.main()
+import calc
+
+
+
+class TestCalc(unittest.TestCase):
+
+
+    def test_add(self):
+
+        self.assertEqual(calc.add(3, 7), 10)
+
+
+
+    def test_is_positive(self):
+
+        self.assertTrue(calc.is_positive(3))
+
+
+
+if __name__ == "__main__":
+    
+    unittest.main()
