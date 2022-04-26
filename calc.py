84605be25f2f27e58285f1744269b81000ed441f
Igor5956 <96588200+Igor5956@users.noreply.github.com>
2022-04-26 18:37:24 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,12 @@
-def add(x, y):
-    return x + y
-def is_positive(x):
+from typing_extensions import Self
+
+def add(x, y):
+    return x + y
+
+def is_positive(x):
     return x > 0
+
+def divide(x, y):
+    if y == 0:
+        raise ZeroDivisionError
+    return x / y
