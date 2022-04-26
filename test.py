c4941a65bd6797c77f607b6c2e2f6707b92d2364
Igor5956 <96588200+Igor5956@users.noreply.github.com>
2022-04-26 18:36:58 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,28 @@
 import unittest
-
 import calc
-
-
+import typing_extensions
 
 class TestCalc(unittest.TestCase):
 
-
     def test_add(self):
-
         self.assertEqual(calc.add(3, 7), 10)
 
-
-
     def test_is_positive(self):
-
         self.assertTrue(calc.is_positive(3))
 
+    def test_divide(self):
+        with self.assertRaises(ZeroDivisionError):
+            calc.divide(8, 0)
 
+    def test_upper(self):
+        self.assertEqual('abc'.upper(), 'ABC')
+
+    def test_split(self):
+        with self.assertRaises(TypeError):
+            'abc'.split(4)
+
+    def test_split(self):
+        self.assertEqual('ab c'.split(), ['ab', 'c'])
 
 if __name__ == "__main__":
     unittest.main()
